a9adb8f17e64e6c1afec5121de9573fbf97a8e92
leonid22231 <lsoldatov.lenya@gmail.com>
2019-10-24 07:11:34 +0700

    Button

--- src/main/java/com/lyadev/Bot.java
+++ src/main/java/com/lyadev/Bot.java
@@ -60,6 +60,7 @@ public class Bot extends TelegramLongPollingBot {
         }else if(update.hasCallbackQuery()){
             String message = update.getCallbackQuery().getData();
             if (message.equals("17")) {
+                //
                 System.out.println("KEKEKEKKEKE");
             }
         }

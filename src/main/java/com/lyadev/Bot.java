4cab95997aa4c9f9cfc7b8b99932464ad503e1eb
leonid22231 <lsoldatov.lenya@gmail.com>
2019-10-25 03:21:47 +0700

    Create DataBase

--- src/main/java/com/lyadev/Bot.java
+++ src/main/java/com/lyadev/Bot.java
@@ -66,9 +66,9 @@ public class Bot extends TelegramLongPollingBot {
                 String message = update.getMessage().getText();
                 String idchat = update.getMessage().getChatId().toString();
                 System.out.println(": " + message);
-                for(int k = 0 ; k < 1000 ;k++ ){
-                    sendMsg(idchat, "Саси"+"["+k+"]");
-                }
+//                for(int k = 0 ; k < 1000 ;k++ ){
+//                    sendMsg(idchat, "Саси"+"["+k+"]");
+//                }
 //                if (admin != null){
 //                    sendMsg(admin, message);
 //            }

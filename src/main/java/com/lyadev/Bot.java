8355c810748594b86c349483f61dc09b390f5b68
leonid22231 <lsoldatov.lenya@gmail.com>
2019-10-25 08:29:57 +0700

    Create DataBase

--- src/main/java/com/lyadev/Bot.java
+++ src/main/java/com/lyadev/Bot.java
@@ -81,7 +81,7 @@ public class Bot extends TelegramLongPollingBot {
                         }else {
                             for(int i = 0 ; i<bd.getUsers().size();i++){
                                 System.out.println(update.getMessage().getFrom().getFirstName() +"||"+ String.valueOf(bd.getUsers().get(i)));
-                                if(update.getMessage().getFrom().getFirstName() == String.valueOf(bd.getUsers().get(i))){
+                                if(update.getMessage().getFrom().getFirstName().equals(String.valueOf(bd.getUsers().get(i)))){
 
                                     user = true;
                                 }
